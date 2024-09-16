22c6e414e4a039ee706f639f68e0f4a9dd4c0544
SamSweden <samedi6774@gmail.com>
2024-09-16 18:49:57 +0200

    Updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <nav>
         <div>
           <Link className="title" to="/">
-            Calculator
+            Smart Calculator
           </Link>
           <div className="menu" onClick={() => setIsMenuOpen(!isMenuOpen)} />
         </div>
